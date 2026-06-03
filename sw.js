f0899b11e13393cf97f3be5ec1968cdd1c2d5626
LloydPWall <lloydpwall@gmail.com>
2026-06-03 11:08:29 +0200

    Update cache version from v42 to v43

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bamnapp-v42';
+const CACHE = 'bamnapp-v43';
 const ASSETS = ['./index.html', './manifest.json', './Kitchen%20Tables.png', './Terrace%20Tables.png'];
 
 self.addEventListener('install', e => {
